47b4582c0c70f681c98d3f4c809b7e90ce41c0fc
bong-dev149 <official.aditya149@gmail.com>
2023-09-02 15:37:35 +0530

    refresh Token controller updated

--- controllers/authControllers/refreshToken.js
+++ controllers/authControllers/refreshToken.js
@@ -17,10 +17,8 @@ const refreshToken = async (req, res) => {
         try {
             user = await tokenController.verifyToken(refreshToken, process.env.JWT_REFRESH_SECRET);
         } catch (err) {
-            
             // clear the cookie in the frontend
-            res.clearCookie('refreshToken');
-            return res.status(401).json({type:'UnauthorizedDevice',msg: 'Invalid refresh token, authorization denied' });
+            return res.clearCookie('refreshToken').status(401).json({type:'UnauthorizedDevice',msg: 'Invalid refresh token, authorization denied' });
         }
 
         // Generate new access token
